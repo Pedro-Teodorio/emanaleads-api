4a16aa9638d1e7e7e33e4352bcd10d48b01c3dbc
Pedro-Teodorio <pedroteodorio92@outlook.com>
2025-12-01 11:28:22 -0300

    style: ajustar a formatação do código para melhor legibilidade

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import { env } from './config/env';
 
 // Para desenvolvimento local
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(env.PORT, () => {
-    console.log(`🚀 Server is running on port ${env.PORT}`);
-  });
+	app.listen(env.PORT, () => {
+		console.log(`🚀 Server is running on port ${env.PORT}`);
+	});
 }
 
 // Para Vercel (serverless)
